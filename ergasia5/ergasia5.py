85ab51cbc8c953331a2ae248acceafe2620b9577
anastasiskalig <61517988+anastasiskalig@users.noreply.github.com>
2020-02-26 22:06:39 +0200

    Update ergasia5.py

--- ergasia5/ergasia5.py
+++ ergasia5/ergasia5.py
@@ -2,6 +2,6 @@ with open('file.txt','r') as f:
   for line in f:
     for word in line.split():
       if len(word) > 3:
-        print (word[1:] + "ay")
+        print (word[1:] +word[0]+ "ay")
 
         
